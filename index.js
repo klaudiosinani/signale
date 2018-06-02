6c32ddc8406142a0e7d932348c7fade8e8d5dc1f
Joydip Roy <joydipand@gmail.com>
2018-06-02 23:05:31 +0545

    fix: lint

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
-const Signale = require('./signale');
 const Badge = require('node-emoji');
-
-console.log(Badge);
+const Signale = require('./signale');
 
 module.exports = Object.assign(new Signale(), {Signale, Badge});
