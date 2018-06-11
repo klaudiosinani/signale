5a4dd0ca32e7a26c9348f425ef71ab8039272b74
Resi Respati <resir014@gmail.com>
2018-06-11 11:08:21 +0700

    rename `test` to `types`, move signale.d.ts inside `types` folder

--- types/signale-tests.ts
+++ types/signale-tests.ts
@@ -2,7 +2,7 @@ import { Signale, SignaleOptions } from "../";
 
 // --- Test 1: Basic Usage --- //
 
-const signale =  new Signale();
+const signale = new Signale();
 
 signale.success("Operation successful");
 signale.debug("Hello", "from", "L59");
@@ -10,29 +10,30 @@ signale.pending("Write release notes for 1.2.0");
 signale.fatal(new Error("Unable to acquire lock"));
 signale.watch("Recursively watching build directory...");
 signale.complete({
-    prefix: "[task]",
-    message: "Fix issue #59",
-    suffix: "(@klauscfhq)"
+  prefix: "[task]",
+  message: "Fix issue #59",
+  suffix: "(@klauscfhq)"
 });
 
 // --- Test 2: Custom Loggers --- //
+
 type CustomLogger = "remind" | "santa";
 
 const optionsCustom: SignaleOptions<CustomLogger> = {
-    stream: process.stdout,
-    scope: "custom",
-    types: {
-        remind: {
-            badge: "**",
-            color: "yellow",
-            label: "reminder"
-        },
-        santa: {
-            badge: "🎅",
-            color: "red",
-            label: "santa"
-        }
+  stream: process.stdout,
+  scope: "custom",
+  types: {
+    remind: {
+      badge: "**",
+      color: "yellow",
+      label: "reminder"
+    },
+    santa: {
+      badge: "🎅",
+      color: "red",
+      label: "santa"
     }
+  }
 };
 
 const custom = new Signale(optionsCustom);
@@ -41,19 +42,20 @@ custom.santa("Hoho! You have an unused variable on L45.");
 custom.debug("This should still work");
 
 // --- Test 3: Overriding Default Loggers --- //
+
 const optionsOverride: SignaleOptions = {
-    types: {
-        error: {
-            badge: "!!",
-            color: "red",
-            label: "fatal error"
-        },
-        success: {
-            badge: "++",
-            color: "green",
-            label: "huge success"
-        }
+  types: {
+    error: {
+      badge: "!!",
+      color: "red",
+      label: "fatal error"
+    },
+    success: {
+      badge: "++",
+      color: "green",
+      label: "huge success"
     }
+  }
 };
 
 signale.error("Default Error Log");
@@ -64,8 +66,9 @@ customOverride.error("Custom Error Log");
 customOverride.success("Custom Success Log");
 
 // --- Test 4: Scoped Loggers --- //
+
 const optionsScope: SignaleOptions = {
-    scope: "global scope"
+  scope: "global scope"
 };
 
 const global = new Signale(optionsScope);
@@ -75,50 +78,51 @@ const global2 = signale.scope("global scope");
 global2.success("Hello from the global scope");
 
 function scopedTest() {
-    const outer = global2.scope("outer", "scope");
-    outer.success("Hello from the outer scope");
+  const outer = global2.scope("outer", "scope");
+  outer.success("Hello from the outer scope");
 
-    setTimeout(() => {
-        const inner = outer.scope("inner", "scope");
-        inner.success("Hello from the inner scope");
-    }, 500);
+  setTimeout(() => {
+    const inner = outer.scope("inner", "scope");
+    inner.success("Hello from the inner scope");
+  }, 500);
 }
 
 scopedTest();
 
 // --- Test 5: Timers --- //
+
 signale.time("test");
 signale.time();
 signale.time();
 
 setTimeout(() => {
-    signale.timeEnd();
-    signale.timeEnd();
-    signale.timeEnd("test");
+  signale.timeEnd();
+  signale.timeEnd();
+  signale.timeEnd("test");
 }, 500);
 
 // --- Test 6: Configuration --- //
 // Overrides any existing `package.json` config
 signale.config({
-    displayFilename: true,
-    displayTimestamp: true,
-    displayDate: false
+  displayFilename: true,
+  displayTimestamp: true,
+  displayDate: false
 });
 
 signale.success("Hello from the Global scope");
 
 function scopedConfigTest() {
-    // `fooLogger` inherits the config of `signale`
-    const fooLogger = signale.scope("foo scope");
+  // `fooLogger` inherits the config of `signale`
+  const fooLogger = signale.scope("foo scope");
 
-    // Overrides both `signale` and `package.json` configs
-    fooLogger.config({
-        displayFilename: true,
-        displayTimestamp: false,
-        displayDate: true
-    });
+  // Overrides both `signale` and `package.json` configs
+  fooLogger.config({
+    displayFilename: true,
+    displayTimestamp: false,
+    displayDate: true
+  });
 
-    fooLogger.success("Hello from the Local scope");
+  fooLogger.success("Hello from the Local scope");
 }
 
 scopedConfigTest();
