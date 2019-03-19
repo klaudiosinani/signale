75a7baec1e82d05e7d4b449f763985ac103a3b33
Klaus Sinani <klaussinani@gmail.com>
2019-03-19 15:25:13 +0200

    Refactored TypeScript ambient declarations

--- types/signale.d.ts
+++ types/signale.d.ts
@@ -1,137 +1,92 @@
-// Type definitions for signale
-// Project: https://github.com/klauscfhq/signale
-// Definitions by: Resi Respati <https://github.com/resir014>
-//                 Kingdaro <https://github.com/kingdaro>
-//                 Joydip Roy <https://github.com/rjoydip>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.7
+/* Authors: Resi Respati <https://github.com/resir014>
+ *          Kingdaro <https://github.com/kingdaro>
+ *          Joydip Roy <https://github.com/rjoydip>
+ *          Klaus Sinani <https://github.com/klaussinani>
+ */
 
-/// <reference types="node" />
+import {Writable as WritableStream} from 'stream';
 
-declare namespace signale {
-  type DefaultMethods =
-    | "await"
-    | "complete"
-    | "error"
-    | "debug"
-    | "fatal"
-    | "fav"
-    | "info"
-    | "note"
-    | "pause"
-    | "pending"
-    | "star"
-    | "start"
-    | "success"
-    | "warn"
-    | "watch"
-    | "log";
+declare namespace _signale {
+  export type DefaultLogger =
+    | 'await'
+    | 'complete'
+    | 'debug'
+    | 'error'
+    | 'fatal'
+    | 'fav'
+    | 'info'
+    | 'log'
+    | 'note'
+    | 'pause'
+    | 'pending'
+    | 'star'
+    | 'start'
+    | 'success'
+    | 'warn'
+    | 'watch';
 
-  interface CommandType {
-    /** The icon corresponding to the logger. */
+  export type LoggerFunction = (...message: any[]) => void;
+
+  export interface LoggerConfiguration {
     badge: string;
-    /**
-     * The color of the label, can be any of the foreground colors supported by
-     * [chalk](https://github.com/chalk/chalk#colors).
-     */
     color: string;
-    /** The label used to identify the type of the logger. */
     label: string;
   }
 
-  interface SignaleConfig {
-    /** Display the scope name of the logger. */
-    displayScope?: boolean;
-    /** Display the badge of the logger. */
+  export interface InstanceConfiguration {
     displayBadge?: boolean;
-    /** Display the current local date in `YYYY-MM-DD` format. */
     displayDate?: boolean;
-    /** Display the name of the file that the logger is reporting from. */
     displayFilename?: boolean;
-    /** Display the label of the logger. */
     displayLabel?: boolean;
-    /** Display the current local time in `HH:MM:SS` format. */
+    displayScope?: boolean;
     displayTimestamp?: boolean;
-    /** Underline the logger label. */
     underlineLabel?: boolean;
-    /** Underline the logger message. */
     underlineMessage?: boolean;
     underlinePrefix?: boolean;
     underlineSuffix?: boolean;
     uppercaseLabel?: boolean;
   }
 
-  interface SignaleOptions<TTypes extends string = DefaultMethods> {
-    /** Sets the configuration of an instance overriding any existing global or local configuration. */
-    config?: SignaleConfig;
+  export interface ConstructorOptions<T extends string> {
+    config?: InstanceConfiguration;
     disabled?: boolean;
-    /**
-     * Name of the scope.
-     */
-    scope?: string;
-    /**
-     * Holds the configuration of the custom and default loggers.
-     */
-    types?: Partial<Record<TTypes, CommandType>>;
     interactive?: boolean;
-    timers?: Map<string, Date>;
-    /**
-     * Destination to which the data is written, can be any valid
-     * [Writable stream](https://nodejs.org/api/stream.html#stream_writable_streams).
-     */
-    stream?: NodeJS.WriteStream;
+    scope?: string;
+    stream?: WritableStream | WritableStream[];
+    types?: Partial<Record<T, LoggerConfiguration>>;
   }
 
-  interface SignaleConstructor {
-    new <TTypes extends string = DefaultMethods>(
-      options?: SignaleOptions<TTypes>
-    ): Signale<TTypes>;
+  export interface Constructor {
+    new <T extends string = DefaultLogger>(
+      options?: ConstructorOptions<T>
+    ): Instance<T>;
   }
 
-  interface SignaleBase<TTypes extends string = DefaultMethods> {
-    /**
-     * Sets the configuration of an instance overriding any existing global or local configuration.
-     *
-     * @param configObj Can hold any of the documented options.
-     */
-    config(configObj: SignaleConfig): Signale<TTypes>;
-    /**
-     * Defines the scope name of the logger.
-     *
-     * @param name Can be one or more comma delimited strings.
-     */
-    scope(...name: string[]): Signale<TTypes>;
-    /** Clears the scope name of the logger. */
+  interface Base<T extends string = DefaultLogger> {
+    config(configObj: InstanceConfiguration): Instance<T>;
+    scope(...name: string[]): Instance<T>;
     unscope(): void;
-    /**
-     * Sets a timers and accepts an optional label. If none provided the timer will receive a unique label automatically.
-     *
-     * @param label Label corresponding to the timer. Each timer must have its own unique label.
-     * @returns a string corresponding to the timer label.
-     */
     time(label?: string): string;
-    /**
-     * Deactivates the timer to which the given label corresponds. If no label
-     * is provided the most recent timer, that was created without providing a
-     * label, will be deactivated.
-     *
-     * @param label Label corresponding to the timer, each timer has its own unique label.
-     * @param span Total running time.
-     */
-    timeEnd(label?: string, span?: number): { label: string; span?: number };
+    timeEnd(label?: string): { label: string; span: number };
   }
 
-  type LoggerFunc = (message?: any, ...optionalArgs: any[]) => void;
-  type Signale<TTypes extends string = DefaultMethods> = SignaleBase<TTypes> &
-    Record<TTypes, LoggerFunc> &
-    Record<DefaultMethods, LoggerFunc>;
+  export type Instance<T extends string = DefaultLogger> = Base<T> &
+    Record<T, LoggerFunction> &
+    Record<DefaultLogger, LoggerFunction>;
 }
 
-declare const signale: signale.Signale<signale.DefaultMethods> & {
-  Signale: signale.SignaleConstructor;
-  SignaleConfig: signale.SignaleConfig;
-  SignaleOptions: signale.SignaleOptions;
-  DefaultMethods: signale.DefaultMethods;
+declare namespace signale {
+  export type DefaultLogger = _signale.DefaultLogger;
+  export type LoggerFunction = _signale.LoggerFunction;
+  export interface SignaleConfiguration
+    extends _signale.InstanceConfiguration {}
+  export interface LoggerConfiguration extends _signale.LoggerConfiguration {}
+  export interface SignaleConstructorOptions<T extends string = DefaultLogger>
+    extends _signale.ConstructorOptions<T> {}
+}
+
+declare const signale: _signale.Instance & {
+  Signale: _signale.Constructor;
 };
 
 export = signale;
